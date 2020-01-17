e9cdacf980779a94d2618051b5b89bbf3c5701e4
Sam Biner <samuel_biner@student.davincischools.org>
2020-01-17 14:40:13 -0800

    Add files via upload

---
+++ stranger_things_theme.rb
@@ -0,0 +1,89 @@
+use_bpm 155
+
+with_fx :reverb, room: 1 do
+  4.times do
+    use_synth :prophet
+    play :C4
+    sleep 0.4
+    play :E4
+    sleep 0.4
+    play :G4
+    sleep 0.4
+    play :D5
+    sleep 0.4
+    play :E5
+    sleep 0.4
+    play :D5
+    sleep 0.4
+    play :G4
+    sleep 0.4
+    play :E4
+    sleep 0.4
+  end
+end
+
+with_fx :reverb, room: 1 do
+  4.times do
+    use_synth :prophet
+    play :B3
+    sleep 0.4
+    play :E4
+    sleep 0.4
+    play :G4
+    sleep 0.4
+    play :D5
+    sleep 0.4
+    play :E5
+    sleep 0.4
+    play :D5
+    sleep 0.4
+    play :G4
+    sleep 0.4
+    play :E4
+    sleep 0.4
+  end
+end
+
+with_fx :reverb, room: 1 do
+  4.times do
+    use_synth :prophet
+    play :C4
+    sleep 0.4
+    play :E4
+    sleep 0.4
+    play :G4
+    sleep 0.4
+    play :D5
+    sleep 0.4
+    play :E5
+    sleep 0.4
+    play :D5
+    sleep 0.4
+    play :G4
+    sleep 0.4
+    play :E4
+    sleep 0.4
+  end
+end
+
+with_fx :reverb, room: 1 do
+  4.times do
+    use_synth :prophet
+    play :B3
+    sleep 0.4
+    play :E4
+    sleep 0.4
+    play :G4
+    sleep 0.4
+    play :D5
+    sleep 0.4
+    play :E5
+    sleep 0.4
+    play :D5
+    sleep 0.4
+    play :G4
+    sleep 0.4
+    play :E4
+    sleep 0.4
+  end
+end
